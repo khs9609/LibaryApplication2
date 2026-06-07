699f983ed2444ea064b3d6666173aec4faa903e8
김효섭 <rlagytjq321@naver.com>
2026-06-08 00:52:44 +0900

    초기 커밋.

---
+++ LibaryApplication2/Context/LibraryDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LibaryApplication2.Models;
+
+namespace LibaryApplication2.Context
+{
+    public class LibraryDb : DbContext
+    {
+        public LibraryDb() : base("name=DBCS") { }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ LibaryApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibaryApplication2.MvcApplication" Language="C#" %>
