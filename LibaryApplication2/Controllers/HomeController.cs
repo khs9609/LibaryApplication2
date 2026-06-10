61c3722608b2a5bed19cfe4780e8d4322e7b50b1
김효섭 <rlagytjq321@naver.com>
2026-06-11 02:05:01 +0900

    ASP.NET MVC 강의 - 도서관 애플리케이션 : 검색 기능 구현

--- LibaryApplication2/Controllers/HomeController.cs
+++ LibaryApplication2/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
+using System.Web.Razor.Tokenizer.Symbols;
 using LibaryApplication2.Context;
 using LibaryApplication2.Models;
 
@@ -18,17 +20,54 @@ namespace LibaryApplication2.Controllers
         {
             int maxListCount = 3;
             int pageNum = 1;
+            string keyword = Request.QueryString["keyword"] ?? string.Empty;
+            string searchKind = Request.QueryString["searchKind"] ?? string.Empty;
+            int totalCount = 0;
+            
 
             if (Request.QueryString["page"] != null)
                 pageNum = Convert.ToInt32(Request.QueryString["page"]);
 
-            var books = db.Books.OrderBy(x => x.Book_U)
+            var books = new List<Book>();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                books = db.Books.OrderBy(x => x.Book_U)
                         .Skip((pageNum - 1) * maxListCount)
                         .Take(maxListCount).ToList();
+                totalCount = db.Books.Count();
+            }
+            else
+            {
+                /* db.Books 는 여러번 사용되서 상단에 따로 정의하고 싶지만 
+                 검색 결과가 많아지는 경우, 속도 저하의 영향을 끼칠 수 있어 
+                 여기선 코드가 길어지더라도 아래와 같이 where 먼저 걸어준다.
+                 */
+                switch (searchKind) {
+                    case "Title":
+                        books = db.Books.Where(x => x.Title.Contains(keyword)).ToList();
+                        totalCount = db.Books.Where(x => x.Title.Contains(keyword)).Count();
+                        break;
+                    case "Writer":
+                        books = db.Books.Where(x => x.Writer.Contains(keyword)).ToList();
+                        totalCount = db.Books.Where(x => x.Writer.Contains(keyword)).Count();
+                        break;
+                    case "Publisher":
+                        books = db.Books.Where(x => x.Publisher.Contains(keyword)).ToList();
+                        totalCount = db.Books.Where(x => x.Publisher.Contains(keyword)).Count();
+                        break;
+                }
+
+                books = books.OrderBy(x => x.Book_U)
+                    .Skip((pageNum - 1) * maxListCount)
+                    .Take(maxListCount).ToList();
+            }
 
             ViewBag.Page = pageNum;
-            ViewBag.TotalCount = db.Books.Count();
+            ViewBag.TotalCount = totalCount;
             ViewBag.MaxListCount = maxListCount;
+            ViewBag.SearchKind = searchKind;
+            ViewBag.Keyword = keyword;
 
             return View(books);
         }
